1b3a09ff677b4ef688b438a126212e28e2aa6c3d
dickeyxxx <jeff@dickeyxxx.com>
2015-04-29 14:22:58 -0700

    refactored out commands dir

--- index.js
+++ index.js
@@ -3,6 +3,17 @@ exports.topics = [{
   description: 'clone an existing app'
 }];
 
-exports.commands = [
-  require('./commands/fork')
-];
+exports.commands = [{
+  topic: 'fork',
+  needsAuth: true,
+  needsApp: true,
+  help: `Fork an existing app -- copy config vars and Heroku Postgres data, and re-provision add-ons to a new app.
+New app name should not be an existing app. The new app will be created as part of the forking process.`,
+  flags: [
+    {name: 'stack', char: 's', description: 'specify a stack for the new app', hasValue: true},
+    {name: 'region', description: 'specify a region', hasValue: true},
+    {name: 'skip-pg', description: 'skip postgres databases', hasValue: false}
+  ],
+  args: [{name: 'newname', optional: true}],
+  run: require('./lib/fork')
+}];
